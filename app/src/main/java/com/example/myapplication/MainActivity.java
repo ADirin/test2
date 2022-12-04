fce59aee5dd323e07b393ebf67aea7351e8fc727
Amir Dirin <amir.dirin@yahoo.com>
2022-12-04 14:56:06 +0200

    Fourth Commit

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -3,6 +3,10 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 /**
  * this is a test
@@ -12,11 +16,23 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 // This is a method
+TextView myText;
 
-    @Override
+Button btnClick;
+EditText edtName;
+
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        edtName = findViewById(R.id.txtName);
+        btnClick = findViewById(R.id.btnName);
+        btnClick.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
 
 
     }
